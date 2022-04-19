98b8eac6516b2dc828403c9d72e6735cf4cc6a36
grafica <paolo@monema.it>
2022-04-19 16:45:09 +0200

    blur image

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,23 @@ import PRODOTTI_DONNA from "../queries/donna";
 
 export default function Home({ data }) {
   console.log(data);
-
+  const shimmer = (w, h) => `
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#fff" offset="20%" />
+        <stop stop-color="#e2e2e2" offset="50%" />
+        <stop stop-color="#fff" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#fff" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`
+  const toBase64 = (str) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
   return (
     <>
       <h1>puppami la fava {data.products.products[0].name}</h1>
@@ -32,6 +48,8 @@ export default function Home({ data }) {
             height={1200}
             layout={"responsive"}
             sizes="50vw"
+            placeholder="blur"
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
           />
         </div>
         <div className="col">
@@ -42,6 +60,8 @@ export default function Home({ data }) {
             height={1200}
             layout={"responsive"}
             sizes="50vw"
+            placeholder="blur"
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
             
           />
         </div>
@@ -53,6 +73,8 @@ export default function Home({ data }) {
             height={1200}
             layout={"responsive"}
             sizes="150vw"
+            placeholder="blur"
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
           />
         </div>
         <div className="col">
@@ -63,6 +85,8 @@ export default function Home({ data }) {
             height={1200}
             layout={"responsive"}
             sizes="50vw"
+            placeholder="blur"
+            blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
           />
         </div>
       </section>
